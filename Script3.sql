fb66c21009a89ebf5169b9ce329d40d865afd179
Gwendal LE GUELLEC <92720133+gwendal56@users.noreply.github.com>
2024-07-29 10:42:22 +0200

    Update Script3.sql

--- Script3.sql
+++ Script3.sql
@@ -7,11 +7,11 @@ LOAD SPATIAL;
 
 -- ============================================ 2023 - World (Fishing China-Taiwan)
 
-CREATE OR REPLACE TABLE world AS
-	SELECT H3Index,	Flag, ShipType, nb
+CREATE OR REPLACE TABLE world_raw AS
+	SELECT * 
 	FROM read_parquet('/Users/gwendalleguellec/Documents/DuckDB/density_flag_type/PARQUET/density.2023.parquet');
 
-CREATE OR REPLACE TABLE world1 AS
+CREATE OR REPLACE TABLE cn_tw AS
 	SELECT 
 		H3Index, 
 		h3_cell_to_boundary_wkt(H3Index).st_geomfromtext() AS geom,
@@ -20,29 +20,29 @@ CREATE OR REPLACE TABLE world1 AS
             WHEN Flag = 'CN' THEN 'red'
             WHEN Flag = 'TW' THEN 'yellow'
         END AS flag_color
-	FROM world
+	FROM world_raw
 	WHERE (flag='CN' OR flag='TW') AND ShipType='fishing'
 	GROUP BY H3Index, geom, nb,	Flag;
 
-ALTER TABLE world ALTER COLUMN H3Index TYPE STRING;
+ALTER TABLE world_raw ALTER COLUMN H3Index TYPE STRING;
 
-CREATE OR REPLACE TABLE world3 AS
+CREATE OR REPLACE TABLE anti_meridian AS
 	SELECT 
 		h3_h3_to_string(H3Index) AS h3,
 		h3_cell_to_lng(h3) AS lng,
 		H3Index, nb
-	FROM world1
+	FROM cn_tw
 	WHERE lng > -177 AND lng < 177;
 
-CREATE OR REPLACE TABLE world2 AS
+CREATE OR REPLACE TABLE fishing_CN_TW AS
 	SELECT geom, nb, Flag, flag_color
-	FROM world1
+	FROM cn_tw
 	WHERE nb = (
-		FROM world3 AS w3
+		FROM anti_meridian AS w3
 		SELECT MAX(nb)
-		WHERE w3.H3Index = world1.H3Index
+		WHERE w3.H3Index = cn_tw.H3Index
 	);
 
-COPY world2
+COPY fishing_CN_TW
 TO '/Users/gwendalleguellec/Documents/DuckDB/density_flag_type/JSON/output/density.2023_fishing_CN_TW.json'
 WITH (FORMAT GDAL, DRIVER 'GeoJSON');
